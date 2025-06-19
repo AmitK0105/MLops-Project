4f4b3427c9a88dd94f81546656d7e7c25fe2ca2a
Amit Kumar <amitk.pr15@gmail.com>
2025-06-19 18:07:28 +0530

    Project flow setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amit Kumar",
+    author_email="amitk.pr15@gmail.com",
+    packages=find_packages()
+)
